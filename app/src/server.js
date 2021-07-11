27619889e030981277e447a79cc31c702b989027
Raul Soledispa <soledispa.raul@gmail.com>
2021-07-10 20:36:53 -0500

    feat(server): Create server

---
+++ app/src/server.js
@@ -0,0 +1,13 @@
+const fastify = require('fastify')({ looger: true });
+
+fastify.get('/api/images', async (req, reply) => ({ data: [] }));
+const start = async () => {
+  try {
+    await fastify.listen(3000);
+  } catch (error) {
+    fastify.log.error(error);
+    process.exit(1);
+  }
+};
+
+start();
