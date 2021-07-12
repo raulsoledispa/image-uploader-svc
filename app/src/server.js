8a3dba9b84492d565d17c5612c05a8baade3beee
Raul Soledispa <soledispa.raul@gmail.com>
2021-07-11 19:56:25 -0500

    feat(server): enable bind to 0.0.0.0

--- app/src/server.js
+++ app/src/server.js
@@ -3,7 +3,7 @@ const serverConfig = require('./config');
 fastify.get('/api/images', async (req, reply) => ({ data: [] }));
 const start = async () => {
   try {
-    await fastify.listen(serverConfig.PORT);
+    await fastify.listen(serverConfig.PORT, '0.0.0.0');
   } catch (error) {
     fastify.log.error(error);
     process.exit(1);
