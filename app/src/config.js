632cb0180652af4cc98d82d258b76d87d2c14ee2
Raul Soledispa <soledispa.raul@gmail.com>
2021-07-11 19:50:55 -0500

    feat(server): add .env file

---
+++ app/src/config.js
@@ -0,0 +1,8 @@
+require('dotenv');
+
+const serverConfig = {
+    PORT: process.env.PORT || 3000
+}
+
+
+module.exports = serverConfig;
--- app/src/server.js
+++ app/src/server.js
@@ -1,9 +1,9 @@
 const fastify = require('fastify')({ logger: true });
-
+const serverConfig = require('./config');
 fastify.get('/api/images', async (req, reply) => ({ data: [] }));
 const start = async () => {
   try {
-    await fastify.listen(3000);
+    await fastify.listen(serverConfig.PORT);
   } catch (error) {
     fastify.log.error(error);
     process.exit(1);
